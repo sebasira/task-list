8d2d805d6c1f1508d83774db8f85348032ff0633
Sebastian M. Irazabal <sebasira@gmail.com>
2016-07-17 14:40:35 -0300

    change angular

--- app/app.js
+++ app/app.js
@@ -6,29 +6,9 @@ var app = angular.module('taskList',[]);
 app.controller('tasksController', function($scope, $http) {
   getTask(); // Load all available tasks 
   function getTask(){  
-  $http.post("ajax/getTask.php").success(function(data){
+  $http.post("ajax/getTasks.php").success(function(data){
         $scope.tasks = data;
        });
   };
-  $scope.addTask = function (task) {
-    $http.post("ajax/addTask.php?task="+task).success(function(data){
-        getTask();
-        $scope.taskInput = "";
-      });
-  };
-  $scope.deleteTask = function (task) {
-    if(confirm("Are you sure to delete this line?")){
-    $http.post("ajax/deleteTask.php?taskID="+task).success(function(data){
-        getTask();
-      });
-    }
-  };
-
-  $scope.toggleStatus = function(item, status, task) {
-    if(status=='2'){status='0';}else{status='2';}
-      $http.post("ajax/updateTask.php?taskID="+item+"&status="+status).success(function(data){
-        getTask();
-      });
-  };
-
+  
 });
