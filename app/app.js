96365532aa5da9cea342ee562718190b4d12bc4b
Sebastian M. Irazabal <sebasira@gmail.com>
2016-07-17 14:43:45 -0300

    change angular

--- app/app.js
+++ app/app.js
@@ -22,6 +22,33 @@ app.controller('tasksController', function($scope, $http){
       $scope.tasks = data;
     });
   };
+
+  // FUNCTION: addTask
+  function addTask(task){
+    $http.psot("ajax/addTask.php?task="+task).success(function(data){
+      getTasks();     // Refresh Task List
+      clearTaskInput();
+    });
+  };
+
+  // FUNCTION: deleteTask
+  function deleteTask(taskID){
+    $http.post("ajax/deleteTask.php?taskID="+taskID).success(function(data){
+      getTasks();     // Refresh Task List
+    });
+  };
+
+  // FUNCTION: toggleStatus
+  function toggleStatus(taskID, status){
+    if (status == '2'){
+      status = '0';
+    }else{
+      status = '2';
+    }
+    $http.post("ajax/updateTask.php?taskID="+taskID+"&status="+status).success(function(data){
+      getTasks();     // Refresh Task List
+    });
+  };
 });
 /*
 // TASK CONTROLLER
