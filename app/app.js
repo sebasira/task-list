5e1acf1c95fcbe503e80ea6563f7062452db5a70
Sebastian M. Irazabal <sebasira@gmail.com>
2016-07-17 14:43:08 -0300

    change angular

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var app = angular.module('taskList',[]);
 
 // TASK CONTROLLER
 //----------------
-app.controller('tasksController', function($scope, $http) {
+app.controller('AtasksController', function($scope, $http) {
   getTask(); // Load all available tasks 
   function getTask(){  
   $http.post("ajax/getTasks.php").success(function(data){
@@ -12,3 +12,55 @@ app.controller('tasksController', function($scope, $http) {
   };
   
 });
+
+app.controller('tasksController', function($scope, $http){
+  getTasks();   // Load available tasks
+
+  // FUNCTION: getTasks
+  function getTasks(){
+    $http.post("ajax/getTasks.php").success(function(data){
+      $scope.tasks = data;
+    });
+  };
+});
+/*
+// TASK CONTROLLER
+//----------------
+app.controller('taskController', function($scope, $http){
+  getTasks();   // Load available tasks
+
+  // FUNCTION: getTasks
+  function getTasks(){
+    $http.post("ajax/getTasks.php").success(function(data){
+      $scope.tasks = data;
+    });
+  };
+
+  // FUNCTION: addTask
+  function addTask(task){
+    $http.psot("ajax/addTask.php?task="+task).success(function(data){
+      getTasks();     // Refresh Task List
+      clearTaskInput();
+    });
+  };
+
+  // FUNCTION: deleteTask
+  function deleteTask(taskID){
+    $http.post("ajax/deleteTask.php?taskID="+taskID).success(function(data){
+      getTasks();     // Refresh Task List
+    });
+  };
+
+  // FUNCTION: toggleStatus
+  function toggleStatus(taskID, status){
+    if (status == '2'){
+      status = '0';
+    }else{
+      status = '2';
+    }
+    $http.post("ajax/updateTask.php?taskID="+taskID+"&status="+status).success(function(data){
+      getTasks();     // Refresh Task List
+    });
+  };
+});
+*/
