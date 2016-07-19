64fd810a6cb3f5eaa1a7a9d21b0be08a2db213bd
Sebastian M. Irazabal <sebasira@gmail.com>
2016-07-19 00:59:27 -0300

    Added delete PHP script

---
+++ ajax/deleteTask.php
@@ -0,0 +1,15 @@
+<?php
+	require_once '../include/db.php';
+
+	if (isset($_GET[taskID])){
+		$taskID = $_GET[taskID];
+
+		$query = "DELETE FROM task WHERE id = 'taskID'";
+
+		$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+		$result = $mysqli->affected_rows;
+
+		echo $json_response = json_encode($result);
+	}
+?>
